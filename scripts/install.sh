a6fe8b3ae0a2b45fc754116c684b517d7cca91f2
thetechprepper <info@thetechprepper.com>
2025-09-30 02:59:51 -0700

    fldigi - Added fldigi (with no PnP) to installer

--- scripts/install.sh
+++ scripts/install.sh
@@ -2,7 +2,7 @@
 #
 # Author  : Gaston Gonzalez
 # Date    : 16 March 2024
-# Updated : 16 August 2025
+# Updated : 30 September          2025
 # Purpose : Main installer for EmComm Tools Community (ETC)
 
 . ./env.sh
@@ -87,3 +87,7 @@ exitIfNotRoot
 
 # Install prediction tools
 ./install-voacap.sh
+
+# Install fldigi suite
+./install-fldigi.sh
+# TODO: add installers for flmsg and flamp
